c9a63c98050ed25f6df703735c4c42eac13c9aed
CJ Burkey <bulletlanguage@gmail.com>
2022-02-12 18:12:52 -0500

    Add GLArea using Glium and Epoxy

--- src/app/imp.rs
+++ src/app/imp.rs
@@ -1,3 +1,5 @@
+use crate::render::GliumGLArea;
+use gtk::ffi::GtkBox;
 use gtk::gdk_pixbuf::PixbufLoader;
 use gtk::gio::SimpleAction;
 use gtk::glib::clone;
@@ -23,7 +25,7 @@ impl ApplicationImpl for FilmFanApp {
         // Get the window template
         let window: ApplicationWindow = builder
             .object("main_window")
-            .expect("could not get object `window` from builder.");
+            .expect("could not get object `window` from builder");
         window.set_application(Some(application));
         window.set_default_size(960, 540);
         window.set_title(Some(&format!(
@@ -45,6 +47,19 @@ impl ApplicationImpl for FilmFanApp {
             println!("open project");
         }));
         window.add_action(&action_open);
+
+        // Get the GL area container box
+        let gl_area_box: Box = builder
+            .object("gl_area_box")
+            .expect("failed to get object `gl_area_box` from builder");
+
+        // Add the GL area into the container
+        let gl_area = GliumGLArea::new();
+        gl_area.set_width_request(256);
+        gl_area.set_height_request(256);
+        gl_area.set_hexpand(true);
+        gl_area.set_vexpand(true);
+        gl_area_box.insert_child_after(&gl_area, Option::<&Box>::None);
 
         // Show the window
         window.present();
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
 mod app;
+mod render;
 
 use app::*;
 use gtk::prelude::*;
+use std::ptr;
 
 fn main() {
+    // Load GL pointers from epoxy (GL context management library used by GTK).
+    {
+        #[cfg(target_os = "macos")]
+        let library = unsafe { libloading::os::unix::Library::new("libepoxy.0.dylib") }.unwrap();
+        #[cfg(all(unix, not(target_os = "macos")))]
+        let library = unsafe { libloading::os::unix::Library::new("libepoxy.so.0") }.unwrap();
+        #[cfg(windows)]
+        let library = libloading::os::windows::Library::open_already_loaded("epoxy-0.dll").unwrap();
+
+        epoxy::load_with(|name| {
+            unsafe { library.get::<_>(name.as_bytes()) }
+                .map(|symbol| *symbol)
+                .unwrap_or(ptr::null())
+        });
+    }
+
     let app = FilmFanApp::new();
     std::process::exit(app.run());
 }
